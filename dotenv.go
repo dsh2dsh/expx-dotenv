dd3f0b592b0a5e45c373069c0ee5e0f35d677bcb
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2023-07-25 22:52:07 +0200

    Initial commit

---
+++ dotenv.go
@@ -0,0 +1,318 @@
+// Package dotenv is a high level wrapper around [godotenv]. It allows to load
+// one or multiple .env file(s) according to [original rules]. It searches for
+// .env file(s) in current and parent dirs, until it find at least one of them.
+//
+// [godotenv]: https://github.com/joho/godotenv
+// [original rules]: https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
+package dotenv
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
+)
+
+// New creates and returns an instance of .env loader [Loader]. By default it
+// searches for .env file(s) until it reaches of the root or any parent dir
+// where go.mod file exists.
+func New() Loader {
+	return Loader{
+		rootDir:   string(filepath.Separator),
+		rootFiles: []string{"go.mod"},
+	}
+}
+
+// Loader is a loader of .env files. Don't create it directly, use [New]
+// instead.
+type Loader struct {
+	// envSuffix is a suffix of .env files for current environment
+	envSuffix string
+
+	// lookupDepth defines how many dirs could be checked before stop. It starts
+	// at 1 and it means current dir only. 2 and more means check also parent
+	// dirs. 0 means not configured.
+	lookupDepth int
+
+	// rootCb is a function, which returns should we stop at current dir or go up.
+	rootCb func(path string) (bool, error)
+
+	// rootDir is a dir to stop and don't go up
+	rootDir string
+
+	// rootFiles contains list of file names for marking root dir. If current or
+	// any parent dir has any of file from this list, we'll stop at that dir.
+	rootFiles []string
+}
+
+// WithDepth configures [Load] don't go up deeper and stop searching for .env
+// files at n level. Current dir has n == 1, first parent dir has n == 2 and so
+// on.
+func (self *Loader) WithDepth(n int) *Loader {
+	self.lookupDepth = n
+	return self
+}
+
+// WithEnvVarName reads name of current environment from s environment variable
+// and configures [Load] for searching and loading of .env.CURENV* files. For
+// instance with s == "production" it'll search also for ".env.production.local"
+// and ".env.production". With s == "test" - ".env.test.local" and
+// ".env.test". And so on.
+func (self *Loader) WithEnvVarName(s string) *Loader {
+	if v, ok := os.LookupEnv(s); ok {
+		self.envSuffix = v
+	}
+	return self
+}
+
+// WithEnvSuffix directly sets name of current environment to s. See
+// [WithEnvVarName] above for details.
+func (self *Loader) WithEnvSuffix(s string) *Loader {
+	self.envSuffix = s
+	return self
+}
+
+// WithRootDir configures [Load] to stop at path dir and don't go up.
+func (self *Loader) WithRootDir(path string) *Loader {
+	if absPath, err := filepath.Abs(path); err == nil {
+		self.rootDir = absPath
+	}
+	return self
+}
+
+// WithRootFiles configures [Load] to stop at current dir or any parent dir,
+// which contains any of file with name from fnames list.
+func (self *Loader) WithRootFiles(fnames ...string) *Loader {
+	self.rootFiles = fnames
+	return self
+}
+
+// WithRootCallback configures [Load] to call fn function for every dir it
+// visits. It passes absolute path of current dir as path param and expects two
+// return values:
+//
+//  1. true means stop at this dir
+//  2. any error
+//
+// [FileExistsInDir] may be usefull in here.
+func (self *Loader) WithRootCallback(fn func(path string) (bool, error),
+) *Loader {
+	self.rootCb = fn
+	return self
+}
+
+// Load loads .env files in current dir if any of them exists. If nothing was
+// found it tries parent dir and parent of parent dir and so on, until it'll
+// find any of .env files or will reach any of configured condition:
+//
+//  1. Visited dir is at level configured by [WithDepth], where level 1 is
+//     current dir, level 2 is parent dir and so on.
+//  2. Visited dir is a root dir configured by [WithRootDir].
+//  3. Visited dir has any of file with names configured by [WithRootFiles].
+//  4. A callback function was configured by [WithRootCallback] and that
+//     function returned true for visited dir.
+//
+// If name of environment wasn't configured by [WithEnvVarName] or
+// [WithEnvSuffix], Load is looking for:
+//
+//  1. env.local
+//  2. .env
+//
+// If name of environment was configured, "production" for instance, it's
+// looking for:
+//
+//  1. .env.production.local
+//  2. .env.local
+//  3. .env.production
+//  4. .env
+//
+// Load uses [godotenv.Load] and according to how it works any already defined
+// env variable can't be redefined by next .env file and has priority. So if
+// variable "A" defined in .env.local file, it can't be redefined by variable
+// "A" from .env file. Or if env variable "A" somehow defined before calling
+// Load, it keeps its value and can't be redefined by .env files.
+func (self *Loader) Load() error {
+	if envs, err := self.lookupEnvFiles(); err != nil {
+		return err
+	} else if envs == nil || len(envs) == 0 {
+		return nil
+	} else if err := godotenv.Load(envs...); err != nil {
+		return fmt.Errorf("can't load %v: %w", envs, err)
+	}
+
+	return nil
+}
+
+// FileExistsInDir checks if file named fname exists in dir named dirName and
+// returns true, if it exists, or false.
+//
+// May be usefull in a callback, configured by [WithRootCallback].
+func (self *Loader) FileExistsInDir(dirName, fname string) (bool, error) {
+	if dirName != "" {
+		fname = filepath.Join(dirName, fname)
+	}
+
+	if _, err := os.Stat(fname); err == nil {
+		return true, nil
+	} else if !errors.Is(err, os.ErrNotExist) {
+		return false, fmt.Errorf("can't stat file '%s': %w", fname, err)
+	}
+
+	return false, nil
+}
+
+// lookupEnvFiles is searching for .env files, starting from current dir, and
+// returns list of found files or nil if nothing found.
+//
+// If .env files were found in one of parent dirs, their names are absolute
+// paths. If they are in current dir, returned list will contain just their
+// names.
+func (self *Loader) lookupEnvFiles() ([]string, error) {
+	envs := self.envFiles()
+
+	found, envDir, err := self.lookupEnvDir(envs)
+	if err != nil {
+		return nil, fmt.Errorf("got error looking for %v: %w", envs, err)
+	} else if !found {
+		return nil, nil
+	}
+
+	// foundEnvs will overwrite envs and it's safe, because we append into
+	// foundEnvs the same number of items or less.
+	foundEnvs := envs[:0]
+	for _, envFile := range envs {
+		if exists, err := self.FileExistsInDir(envDir, envFile); err != nil {
+			return nil, err
+		} else if exists {
+			if envDir != "" {
+				envFile = filepath.Join(envDir, envFile)
+			}
+			foundEnvs = append(foundEnvs, envFile)
+		}
+	}
+
+	if len(foundEnvs) > 0 {
+		return foundEnvs, nil
+	}
+
+	return nil, nil // nothing found
+}
+
+// envFile returns list of .env files for searching, according to configured
+// name of environment. See [Load] for details.
+func (self *Loader) envFiles() []string {
+	envName := self.envSuffix
+	if envName == "" {
+		return []string{".env.local", ".env"}
+	}
+
+	return []string{
+		".env." + envName + ".local", ".env.local",
+		".env." + envName, ".env",
+	}
+}
+
+// lookupEnvDir is searching for a dir, which contains any of files with names
+// from envFiles list. It returns:
+//
+//  1. true, if found any of files, or false.
+//  2. dir name if any file was found
+//  3. any error
+//
+// Returned dir name is absolute path or empty string, which means current dir.
+//
+// It starts searching at current dir, next tries parent dir, parent of parent
+// dir and so on, until it reaches configured root.
+func (self *Loader) lookupEnvDir(envFiles []string) (bool, string, error) {
+	curDir := ""
+	depth := 0
+
+	for {
+		for _, envFile := range envFiles {
+			if exists, err := self.FileExistsInDir(curDir, envFile); err != nil {
+				return false, "", err
+			} else if exists {
+				return exists, curDir, nil
+			}
+		}
+
+		if depth = self.checkLookupDepth(depth); depth < 0 {
+			break
+		}
+
+		if newDir, err := self.nextParentDir(curDir); err != nil {
+			return false, "", fmt.Errorf("next parent dir of %v: %w", curDir, err)
+		} else if newDir == "" {
+			break
+		} else {
+			curDir = newDir
+		}
+	}
+
+	return false, "", nil
+}
+
+// checkLookupDepth compares current dir level curDir with configured one and
+// returns -1, if reached configured limit, or next level. It expects curDir >=
+// 0.
+//
+// It understands the limit is configured if lookupDepth > 0.
+func (self *Loader) checkLookupDepth(curDepth int) int {
+	if self.lookupDepth > 0 {
+		curDepth++
+		if curDepth == self.lookupDepth {
+			return -1
+		}
+	}
+
+	return curDepth
+}
+
+// nextParentDir returns parent dir of curDir or empty string, if it configured
+// to stop at curDir. It expects curDir is an absolute path or empty string,
+// which means current dir.
+func (self *Loader) nextParentDir(curDir string) (string, error) {
+	if curDir == "" {
+		if dir, err := os.Getwd(); err != nil {
+			return "", fmt.Errorf("can't get current dir: %w", err)
+		} else {
+			curDir = dir
+		}
+	}
+
+	if stopHere, err := self.stopByRootCb(curDir); err != nil {
+		return "", err
+	} else if stopHere {
+		return "", nil
+	} else if curDir == self.rootDir {
+		return "", nil
+	}
+
+	for _, fname := range self.rootFiles {
+		if exists, err := self.FileExistsInDir(curDir, fname); err != nil {
+			return "", fmt.Errorf("check existance of file %v in dir %v: %w", fname,
+				curDir, err)
+		} else if exists {
+			return "", nil
+		}
+	}
+
+	return filepath.Dir(curDir), nil
+}
+
+// stopByRootCb calls a function, configured by [WithRootCallback], with
+// absolute path, and returns its return values. true means stop at this path
+// and false means continue to parent dir.
+func (self *Loader) stopByRootCb(path string) (bool, error) {
+	if self.rootCb != nil {
+		if stopHere, err := self.rootCb(path); err != nil {
+			return false, fmt.Errorf("check dir %v using root callback: %w", path, err)
+		} else {
+			return stopHere, nil
+		}
+	}
+
+	return false, nil
+}
---
+++ dotenv_test.go
@@ -0,0 +1,392 @@
+package dotenv
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func valueNoError[V any](t *testing.T) func(val V, err error) V {
+	return func(val V, err error) V {
+		require.NoError(t, err)
+		return val
+	}
+}
+
+func TestWithDepth(t *testing.T) {
+	env := New()
+	assert.Equal(t, env.lookupDepth, 0)
+	assert.Same(t, &env, env.WithDepth(env.lookupDepth+1))
+	assert.Equal(t, env.lookupDepth, 1)
+}
+
+func TestWithEnvVarName(t *testing.T) {
+	env := New()
+	assert.Equal(t, "", env.envSuffix)
+	t.Setenv("ENV", "123")
+	assert.Same(t, &env, env.WithEnvVarName("ENV"))
+	assert.Equal(t, "123", env.envSuffix)
+}
+
+func TestWithEnvSuffix(t *testing.T) {
+	env := New()
+	assert.Equal(t, "", env.envSuffix)
+	assert.Same(t, &env, env.WithEnvSuffix("123"))
+	assert.Equal(t, "123", env.envSuffix)
+}
+
+func TestWithRootDir(t *testing.T) {
+	env := New()
+	assert.Equal(t, string(filepath.Separator), env.rootDir)
+
+	curDir := valueNoError[string](t)(os.Getwd())
+	parentDir := valueNoError[string](t)(filepath.Abs("../"))
+
+	tests := []struct {
+		withRootDir string
+		expect      string
+	}{
+		{
+			withRootDir: curDir,
+			expect:      curDir,
+		},
+		{
+			withRootDir: "./",
+			expect:      curDir,
+		},
+		{
+			withRootDir: "./abc",
+			expect:      filepath.Join(curDir, "abc"),
+		},
+		{
+			withRootDir: "../",
+			expect:      parentDir,
+		},
+		{
+			withRootDir: "testdata/a",
+			expect:      filepath.Join(curDir, "testdata", "a"),
+		},
+	}
+
+	for _, tt := range tests {
+		assert.Same(t, &env, env.WithRootDir(tt.withRootDir))
+		assert.Equal(t, tt.expect, env.rootDir)
+	}
+}
+
+func TestWithRootFiles(t *testing.T) {
+	env := New()
+	assert.Equal(t, []string{"go.mod"}, env.rootFiles)
+	env.WithRootFiles(".git", "go.mod")
+	assert.Equal(t, []string{".git", "go.mod"}, env.rootFiles)
+}
+
+func TestWithRootCallback(t *testing.T) {
+	env := New()
+	assert.Nil(t, env.rootCb)
+	assert.False(t, valueNoError[bool](t)(env.stopByRootCb("")))
+
+	var count int
+	env.WithRootCallback(func(path string) (bool, error) {
+		count++
+		return true, nil
+	})
+	assert.NotNil(t, env.rootCb)
+	assert.True(t, valueNoError[bool](t)(env.stopByRootCb("")))
+	assert.Equal(t, 1, count)
+
+	env.WithRootCallback(func(path string) (bool, error) {
+		return false, os.ErrNotExist
+	})
+	_, err := env.stopByRootCb("")
+	assert.ErrorIs(t, err, os.ErrNotExist)
+
+	env.WithRootCallback(func(path string) (bool, error) {
+		return path == "/", nil
+	})
+	assert.False(t, valueNoError[bool](t)(env.stopByRootCb("")))
+	assert.True(t, valueNoError[bool](t)(env.stopByRootCb("/")))
+}
+
+func TestFileExistsInDir(t *testing.T) {
+	hasFile := "dotenv_test.go"
+
+	tests := []struct {
+		dir    string
+		file   string
+		exists bool
+	}{
+		{
+			dir:    "",
+			file:   hasFile,
+			exists: true,
+		},
+		{
+			dir:    "./",
+			file:   hasFile,
+			exists: true,
+		},
+		{
+			dir:    "testdata",
+			file:   ".env",
+			exists: true,
+		},
+		{
+			dir:    "",
+			file:   "not exists",
+			exists: false,
+		},
+	}
+
+	env := New()
+	for _, tt := range tests {
+		exists, err := env.FileExistsInDir(tt.dir, tt.file)
+		require.NoError(t, err)
+		if tt.exists {
+			assert.True(t, exists, "expected file %v exists in dir %v",
+				tt.file, tt.dir)
+		} else {
+			assert.False(t, exists, "not expected file %v exists in dir %v",
+				tt.file, tt.dir)
+		}
+	}
+}
+
+func TestLoader_envFiles(t *testing.T) {
+	env := New()
+	assert.Equal(t, []string{".env.local", ".env"}, env.envFiles())
+
+	env.WithEnvSuffix("test")
+	assert.Equal(t,
+		[]string{".env.test.local", ".env.local", ".env.test", ".env"},
+		env.envFiles())
+}
+
+func TestLoader_checkLookupDepth(t *testing.T) {
+	tests := []struct {
+		name        string
+		lookupDepth int
+		curDepth    int
+		expect      int
+	}{
+		{
+			name:        "with negative lookupDepth",
+			lookupDepth: -1,
+			expect:      0,
+		},
+		{
+			name:        "with zero lookupDepth",
+			lookupDepth: 0,
+			expect:      0,
+		},
+		{
+			name:        "with positive lookupDepth and zero curDepth",
+			lookupDepth: 2,
+			expect:      1,
+		},
+		{
+			name:        "with positive lookupDepth and one curDepth",
+			lookupDepth: 2,
+			curDepth:    1,
+			expect:      -1,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			env := New()
+			env.WithDepth(tt.lookupDepth)
+			assert.Equal(t, tt.expect, env.checkLookupDepth(tt.curDepth))
+		})
+	}
+}
+
+func TestLoad(t *testing.T) {
+	curDir := valueNoError[string](t)(os.Getwd())
+	t.Cleanup(func() {
+		require.NoError(t, os.Chdir(curDir))
+	})
+
+	const testEnvVar = "TEST_VAR1"
+	const testEnvVal = "testdata"
+
+	tests := []struct {
+		name       string
+		dir        string
+		envVarName string
+		expect     string
+		expectErr  bool
+		before     func(*testing.T, *Loader)
+	}{
+		{
+			name:       "in root dir",
+			dir:        "testdata",
+			envVarName: testEnvVar,
+			expect:     testEnvVal,
+		},
+		{
+			name:       "in subdir",
+			dir:        "testdata/a",
+			envVarName: testEnvVar,
+			expect:     testEnvVal,
+		},
+		{
+			name:       "with test suffix",
+			dir:        "testdata/a",
+			envVarName: testEnvVar,
+			expect:     "testdata-test",
+			before: func(t *testing.T, env *Loader) {
+				env.WithEnvSuffix("test")
+			},
+		},
+		{
+			name:       "with test suffix but from .env",
+			dir:        "testdata/a",
+			envVarName: "TEST_VAR2",
+			expect:     "testdata2",
+			before: func(t *testing.T, env *Loader) {
+				env.WithEnvSuffix("test")
+			},
+		},
+		{
+			name:       "with test ENV",
+			dir:        "testdata/a",
+			envVarName: testEnvVar,
+			expect:     "testdata-test",
+			before: func(t *testing.T, env *Loader) {
+				t.Setenv("ENV", "test")
+				env.WithEnvVarName("ENV")
+			},
+		},
+		{
+			name:       "WithRootDir empty dir",
+			dir:        "testdata/a",
+			envVarName: testEnvVar,
+			expect:     "",
+			before: func(t *testing.T, env *Loader) {
+				curDir := valueNoError[string](t)(os.Getwd())
+				env.WithRootDir(curDir)
+			},
+		},
+		{
+			name:       "WithRootDir testdata",
+			dir:        "testdata/a",
+			envVarName: testEnvVar,
+			expect:     testEnvVal,
+			before: func(t *testing.T, env *Loader) {
+				curDir := valueNoError[string](t)(os.Getwd())
+				env.WithRootDir(filepath.Dir(curDir))
+			},
+		},
+		{
+			name:       "WithDepth 1",
+			dir:        "testdata/a",
+			envVarName: testEnvVar,
+			expect:     "",
+			before: func(t *testing.T, env *Loader) {
+				env.WithDepth(1)
+			},
+		},
+		{
+			name:       "WithDepth 2",
+			dir:        "testdata/a",
+			envVarName: testEnvVar,
+			expect:     testEnvVal,
+			before: func(t *testing.T, env *Loader) {
+				env.WithDepth(2)
+			},
+		},
+		{
+			name:       "with error from Load",
+			dir:        "testdata",
+			envVarName: testEnvVar,
+			expectErr:  true,
+			before: func(t *testing.T, env *Loader) {
+				env.WithEnvSuffix("error")
+			},
+		},
+		{
+			name:       "WithRootCb stop at a",
+			dir:        "testdata/a",
+			envVarName: testEnvVar,
+			expect:     "",
+			before: func(t *testing.T, env *Loader) {
+				env.WithRootCallback(func(path string) (bool, error) {
+					return true, nil
+				})
+			},
+		},
+		{
+			name:       "WithRootCb stop at testdata",
+			dir:        "testdata/a",
+			envVarName: testEnvVar,
+			expect:     "testdata",
+			before: func(t *testing.T, env *Loader) {
+				curDir := valueNoError[string](t)(os.Getwd())
+				env.WithRootCallback(func(path string) (bool, error) {
+					return path == filepath.Dir(curDir), nil
+				})
+			},
+		},
+		{
+			name:       "WithRootCb error",
+			dir:        "testdata/a",
+			envVarName: testEnvVar,
+			expectErr:  true,
+			before: func(t *testing.T, env *Loader) {
+				env.WithRootCallback(func(path string) (bool, error) {
+					return false, os.ErrNotExist
+				})
+			},
+		},
+		{
+			name:       "WithRootFiles stop at go.mod",
+			dir:        "testdata/b",
+			envVarName: testEnvVar,
+			expect:     "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.NoError(t, os.Chdir(tt.dir))
+			t.Cleanup(func() {
+				require.NoError(t, os.Chdir(curDir))
+			})
+			t.Setenv(tt.envVarName, "")
+			require.NoError(t, os.Unsetenv(tt.envVarName))
+			env := New()
+			if tt.before != nil {
+				tt.before(t, &env)
+			}
+			if tt.expectErr {
+				require.Error(t, env.Load())
+			} else {
+				require.NoError(t, env.Load())
+				assert.Equal(t, tt.expect, os.Getenv(tt.envVarName),
+					"unexpected value of %v in %v dir", tt.envVarName, tt.dir)
+
+			}
+		})
+	}
+}
+
+func TestLoad_errorGetwd(t *testing.T) {
+	tmpDir := valueNoError[string](t)(os.MkdirTemp("", "expx-dotenv-"))
+	t.Cleanup(func() {
+		require.NoError(t, os.RemoveAll(tmpDir))
+	})
+
+	curDir := valueNoError[string](t)(os.Getwd())
+	require.NoError(t, os.Chdir(tmpDir))
+	t.Cleanup(func() {
+		require.NoError(t, os.Chdir(curDir))
+	})
+
+	env := New()
+	require.NoError(t, os.RemoveAll(tmpDir))
+	assert.Error(t, env.Load())
+}
